a410ef4b0146ac1318aa8e39d44ca309ab458370
younes_qasempour <qasempouryounes@gmail.com>
2025-03-13 23:43:04 +0330

    Working

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-
 app = Flask(__name__)
 
 all_books = []
 
-
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html", books=all_books)
 
-
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
-
+    if request.method == "POST":
+        new_book = {
+            "title": request.form.get("title", ""),
+            "author": request.form.get("author", ""),
+            "rating": request.form.get("rating", "")
+        }
+        all_books.append(new_book)
+        return redirect(url_for('home'))
+    return render_template("add.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
