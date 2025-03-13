0c62ce2063e1cc7c53f43e8bcb37c90ae48ae700
younes_qasempour <qasempouryounes@gmail.com>
2025-03-14 02:39:48 +0330

    final

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    result = db.session.execute(db.select(Book).order_by(Book.title)).scalars().all()
+    return render_template("index.html", books=result)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
@@ -44,6 +45,29 @@ def add():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form.get("id", "")
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = float(request.form.get("rating", ""))
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+
+@app.route('/delete')
+def delete():
+    book_id = request.args.get('id')
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
